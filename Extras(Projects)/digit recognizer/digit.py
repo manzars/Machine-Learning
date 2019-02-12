b6f79f0d68dd4ccdfb92aceb43bd3b7873abd9dd
manzars <manzarshaikh69@gmail.com>
2019-02-13 00:37:33 +0530

    Project updated

--- Extras(Projects)/digit recognizer/digit.py
+++ Extras(Projects)/digit recognizer/digit.py
@@ -11,26 +11,25 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix
 import numpy as np
+import pandas as pd
 
-#loading the dataset of Digits of 8X8 matrix of gray scale
-digit = load_digits()
-data = digit.data
-image = digit.images # this variable has 1797 8X8 matrix for pixel 
-feature = digit.target # this variable has unique output
-
-# Seperating 
-X_train, X_test, y_train, y_test = train_test_split(data, feature, test_size = 0.25)
-classifier = RandomForestClassifier(n_estimators = 100, criterion = "entropy", n_jobs = 3, random_state = 0)
-classifier.fit(X_train, y_train)
-pred = classifier.predict(X_test)
-print("Predicted digit = {}".format(classifier.predict(digit.data[786].reshape(1,64))))
+data = pd.read_csv("train.csv")
 plt.gray()
-plt.matshow(digit.images[786])
+plt.matshow(X[33].reshape(28,28))
 plt.show()
-cm = confusion_matrix(y_test, pred)
 
-from PIL import Image
-img = np.asarray(Image.open('abc.jpg').convert('LA'))
-img.save('greyscale.png')
+y = data.iloc[:, 0:1].values
+X = data.iloc[:, 1:].values
 
-import opencv
+classifier = RandomForestClassifier(n_estimators = 100, n_jobs = 3, criterion = "entropy", random_state = 0)
+classifier.fit(X,y)
+classifier.predict(X[33].reshape(1,784))
+
+X_test = pd.read_csv("test.csv")
+X_test = X_test.values
+pred = classifier.predict(X)
+
+cm = confusion_matrix(y, pred)
+
+
+            
